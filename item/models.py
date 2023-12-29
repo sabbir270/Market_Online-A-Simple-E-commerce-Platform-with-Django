45f9d2cadd76761bb5dca3ccfc9fe8798168b6fa
sabbir <sabbirsarkar27@gmail.com>
2023-12-29 11:48:10 +0600

    completed

---
+++ item/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    class Meta:
+        ordering=('name',)
+        verbose_name_plural = 'Categories'  # Corrected spelling here
+
+    def __str__(self):
+        return self.name
+class Item(models.Model):
+    category=models.ForeignKey(Category,related_name='items',on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description=models.TextField(blank=True,null=True)
+    price=models.FloatField()
+    image=models.ImageField(upload_to='item_image',blank=True,null=True)
+    is_sold=models.BooleanField(default=False)
+    created_by=models.ForeignKey(User,related_name='items', on_delete=models.CASCADE)
+    created_at=models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.name
